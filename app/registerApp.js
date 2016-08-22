3b5406a6640528d49c0e5a4dafb33945bd142df1
tejaskb04 <tejaskb@gmail.com>
2016-08-21 22:46:24 -0700

    post code

--- app/registerApp.js
+++ app/registerApp.js
@@ -12,7 +12,25 @@ app.controller("registerController",
                 console.log("register submit called");
                 window.alert(this.viewModel.account);
                 window.alert(this.viewModel.password);
-            }
+                var url = "http://localhost:4000/register";// get this from BASEURL_PYRAMID
+                $http({
+                    method: "POST",
+                    url: url,
+                    data: $scope.viewModel,  // pass in data as JSON
+                    headers: {"Content-Type": "application/json"}
+                })
+                .then(
+                    function (response) {
+                        console.log("success response: " + response);
+                        window.alert("success response: " + response);
+                    },
+
+                    function (response) {
+                        console.log("error response: " + response);
+                        window.alert("error response: " + response);
+                    }
+                );
+            };
         }
     ]
 );
